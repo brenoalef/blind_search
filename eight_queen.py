11e8be9825895b6408f9a83e503699e6b2874c3c
brenoalef <b.alefdsa@gmail.com>
2018-08-05 20:18:00 -0300

    Optimizing choice of actions

--- eight_queen.py
+++ eight_queen.py
@@ -3,8 +3,6 @@ from math import ceil
 from tkinter import *
 
 class EightQueen:
-    board = range(1, 65)
-
     def __init__(self, initialState):
         self.initialState = initialState
 
@@ -12,22 +10,48 @@ class EightQueen:
         return len(state) == 8
 
     def actions(self, state):
-        not_allowed = []
+        col = len(state) + 1
+        available = range(col, 65, 8)
         for p in state:
             l = ceil(p / 8)
-            c = 1 + (p-1) % 8
-            not_allowed += range(c, 65, 8)
-            not_allowed += range((l-1)*8 + 1, l*8 + 1, 1)
-            not_allowed += range(p - (min(l, c) - 1)*9, p + (8 - max(l, c))*9 + 1, 9)
-            not_allowed += range(p - (min(l - 1, 8 - c))*7, p + min(8 - l, c - 1)*7 + 1, 7)
-        return [ x for x in self.board if not x in not_allowed ]
+            c = 1 + (p - 1) % 8
+            not_allowed = [(p + col - c), (p + (col - c) * 9), (p - (col - c) * 7)]
+            available = [x for x in available if not x in not_allowed]
+        return available
 
-
-    def result(self, state, action):
+    def result(self, state, action):      
         return state + [action]
 
     def step_cost(self, state, action):
         return 1
+
+
+def show(positions):
+    root = Tk()
+    root.title('8-Queens')
+    canvas = Canvas(root, bg = 'white', height = 500, width = 500)
+    canvas.pack(side = TOP, padx = 10, pady = 10)
+    queen = PhotoImage(file = "queen.gif")
+    queen = queen.subsample(8, 8)
+    x =1
+    y =1
+    square_size = 500/8
+    for rows in range(8):
+        color_white = not (rows % 2)
+        for columns in range(8):
+            color = "lightgray" if color_white else "red"
+            x = columns * square_size
+            y = rows * square_size
+            canvas.create_rectangle(x, y, x + square_size, y + square_size, fill = color)
+            if rows * 8 + columns + 1 in positions:
+                canvas.create_image(x, y, anchor = NW, image = queen)
+            color_white = not color_white
+
+    bou1 = Button(root, text = 'Close', width = 25, command = root.quit)
+    bou1.pack(side = RIGHT, padx = 10, pady = 10)
+
+    root.mainloop()
+
 
 problem = EightQueen([])
 print(dfs(problem))
@@ -37,34 +61,10 @@ print(ids(problem))
 print(bfs(problem))
 print(uniform_cost(problem))
 
-
-result = dls(problem, 8)
-print(result)
-root = Tk()
-root.title(str(8) + ' queens')
-canvas = Canvas(root,bg='white',height=500,width=500)
-canvas.pack(side=TOP,padx=10,pady=10)
-queen = PhotoImage(file="queen.gif")
-queen = queen.subsample(8, 8)
-board_rows=8
-board_cols=8
-x=1
-y=1
-square_size= 500/8
-for rows in range(board_rows):
-    color_white = not (rows%2)
-    for columns in range(board_cols):
-        color="lightgray"
-        if not color_white:
-            color="red"
-        x=columns*square_size
-        y=rows*square_size
-        canvas.create_rectangle(x, y, x+square_size, y+square_size, fill=color)
-        if rows*8 + columns + 1 in result:
-            canvas.create_image(x, y, anchor = NW, image=queen)
-        color_white = not color_white
-
-bou1 = Button(root,text='Close',width=25,command=root.quit)
-bou1.pack(side=RIGHT,padx=10,pady=10)
-
-root.mainloop()
+result = dfs(problem)
+#result = dfs_visited(problem)
+#result = dls(problem, 8)
+#result = ids(problem)
+#result = bfs(problem)
+#result = uniform_cost(problem)
+show(result)
